a716f0f0abdc4cf6d5feb396e4a6e40082a0bca0
circular arc <tridath3@gmail.com>
2022-05-30 11:27:56 +0000

    .

--- src/main.py
+++ src/main.py
@@ -1,21 +1,41 @@
-from lib2to3.pgen2 import token
 import discord
-from discord.ext import commands
+import interactions
 import python_weather
 
-bot = commands.Bot(command_prefix='!', description='testing')
+file = open(r"src/token", "r")
+token = file.read()
+file.close()
 
-@bot.event
-async def on_ready():
-    print('Logged in as', bot.user.name, end=", ")
-    print(bot.user.id)
+bot = interactions.Client(token=token)
 
-@commands.command()
-async def ping(ctx):
-    await ctx.send(f'pong! {round(round(bot.latency, 4)*1000)}ms')
+@bot.command(
+    name="ping",
+    description="ping",
+    scope=854519080882143262
+)
+async def ping(ctx: interactions.CommandContext):
+    await ctx.send(f'pong! {round(round(bot.latency, 4))}ms')
 
-@commands.command()
-async def weather(ctx, day="", *, city):
+@bot.command(
+    name="weather",
+    description="weather",
+    scope=854519080882143262,
+    options=[
+        interactions.Option(
+            name="day",
+            description="yesterday, today, tomorrow",
+            type=interactions.OptionType.STRING,
+            required=True
+        ),
+        interactions.Option(
+            name="city",
+            description="Where do you live?",
+            type=interactions.OptionType.STRING,
+            required=True
+        )
+    ]
+)
+async def weather(ctx, day: str, *, city: str):
     client = python_weather.Client(format=python_weather.METRIC)
     w = await client.find(city)
     yesterday = w.forecasts[0]
@@ -23,31 +43,17 @@ async def weather(ctx, day="", *, city):
     tomorrow = w.forecasts[2]
     print(day, w)
     if day == "yesterday":
-        await ctx.send(f'{yesterday.date.strftime("%Y-%m-%d")} {yesterday.sky_text} {yesterday.temperature}')
-        await ctx.send(f'{w.location_name}')
-        await ctx.send(f'{w.url}')
+        await ctx.send(f'{yesterday.date.strftime("%Y-%m-%d")} {yesterday.sky_text} {yesterday.temperature}°C\n{w.location_name}\n{w.url}')
     elif day == "current":
-        await ctx.send(f'{w.current.date.strftime("%H:%M")} {w.current.temperature}°C')
-        await ctx.send(f'{w.location_name}')
-        await ctx.send(f'{w.url}')
+        await ctx.send(f'{w.current.date.strftime("%H:%M")} {w.current.temperature}°C\n{w.location_name}\n{w.url}')
     elif day == "today":
-        await ctx.send(f'{today.date.strftime("%Y-%m-%d")} {today.sky_text} {today.temperature}')
-        await ctx.send(f'{w.location_name}')
-        await ctx.send(f'{w.url}')
+        await ctx.send(f'{today.date.strftime("%Y-%m-%d")} {today.sky_text} {today.temperature}°C\n{w.location_name}\n{w.url}')
     elif day == "tomorrow":
-        await ctx.send(f'{tomorrow.date.strftime("%Y-%m-%d")} {tomorrow.sky_text} {tomorrow.temperature}')
-        await ctx.send(f'{w.location_name}')
-        await ctx.send(f'{w.url}')
+        await ctx.send(f'{tomorrow.date.strftime("%Y-%m-%d")} {tomorrow.sky_text} {tomorrow.temperature}°C\n{w.location_name}\n{w.url}')
     else:
         for forecast in w.forecasts:
             # print(forecast)
-            await ctx.send(f'{forecast.date.strftime("%Y-%m-%d")}: {forecast.sky_text} {forecast.temperature}°C')
-        await ctx.send(f'{w.location_name}')
-        await ctx.send(f'{w.url}')
+            await ctx.send(f'{forecast.date.strftime("%Y-%m-%d")}: {forecast.sky_text} {forecast.temperature}°C\n{w.location_name}\n{w.url}')
     await client.close()
 
-file = open(r"src/token", "r")
-token = file.read()
-file.close()
-
-bot.run(token)
+bot.start()
